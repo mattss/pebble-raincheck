0dd810eee8b4ff2400d2e553987780103a93aa6b
Matthew Sital-Singh <mattss@netsight.co.uk>
2015-04-17 14:18:34 +0100

    Location lookup added

--- src/app.js
+++ src/app.js
@@ -1,41 +1,71 @@
 var UI = require('ui');
+var ajax = require('ajax');
+var Accel = require('ui/accel');
 
-// Create a Card with title and subtitle
+var dark_sky_api_key = '46bdaa3f66331d12897ed8474accd381';
+var location_name = 'Bristol';
+
+// Set up the main Card for display
 var card = new UI.Card({
   title:'Rain Check',
   body:'Fetching data...'
 });
-
-// Display the Card
 card.show();
 
-// make request to dark sky
-var ajax = require('ajax');
+Accel.init();
 
-// Construct URL
-var api_key = '46bdaa3f66331d12897ed8474accd381';
-var latlong = '48.1333,11.5667';
-var URL = 'https://api.forecast.io/forecast/' + api_key + '/' + latlong;
+function get_location(locname) {
+  console.log('Getting location data for: ' + locname);
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + locname;
+  ajax(
+    {
+      url: url,
+      type: 'json'
+    },
+    function(data) {
+      // Success!
+      console.log('Successfully fetched location data!');
+      if (data.results.length === 0) {
+        console.log('No results for location:' + locname);
+        card.body('Unknown location:' + locname);
+        return;
+      }
+      var result = data.results[0];
+      console.log('Location match: ' + result.formatted_address);
+      card.body('Fetching data for ' + result.formatted_address);
+      var geo = result.geometry.location;
+      console.log('lat/lng:' + geo);
+      update_weather_data(geo.lat, geo.lng);
+    },
+    function(error) {
+      // Failure!
+      console.log('Failed fetching location data: ' + error);
+    }
+  );
+}
 
-// Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    // Success!
-    console.log('Successfully fetched weather data!');
-    var weather_text = parseData(data);
-    card.body(weather_text);
-  },
-  function(error) {
-    // Failure!
-    console.log('Failed fetching weather data: ' + error);
-  }
-);
+function update_weather_data(lat, lng) {
+  var ds_URL = 'https://api.forecast.io/forecast/' + dark_sky_api_key + '/' + lat + ',' + lng;
+  console.log('Looking up weather data from' + ds_URL);
+  ajax(
+    {
+      url: ds_URL,
+      type: 'json'
+    },
+    function(data) {
+      // Success!
+      console.log('Successfully fetched weather data!');
+      var weather_text = parse_weather_data(data);
+      card.body(weather_text);
+    },
+    function(error) {
+      // Failure!
+      console.log('Failed fetching weather data: ' + error);
+    }
+  );
+}
 
-function parseData(data) {
+function parse_weather_data(data) {
   var items = data.hourly.data;
   for (var i=0; i<items.length; i++) {
     var item = items[i];
@@ -58,6 +88,11 @@ function parseData(data) {
     }
   }
   return 'No rain today.';
-  card.body(data.currently.summary);
-
 }
+
+get_location(location_name);
+
+card.on('accelTap', function(e) {
+  console.log('TAP!');
+  get_location(location_name);
+});
